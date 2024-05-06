6b36b19142cc1e3a5b55340cd47b11cbeca6ea24
tej <161642007+tejm999@users.noreply.github.com>
2024-05-06 16:39:07 -0400

    init

---
+++ src/Color.java
@@ -0,0 +1,14 @@
+
+public enum Color {WHITE,BLACK;
+
+    public void setPiece(int x, int y, Piece other) {
+    }
+
+    public Piece getPiece(int x, int y) {
+
+        return null;
+    }
+
+    public void add(Piece other) {
+    }
+}
